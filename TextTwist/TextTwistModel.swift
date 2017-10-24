318705371c53d8fbec07d8b2a0fd9a1ba324e22a
Yaseen-al <yaseenaldellesh@ac.c4q.nyc>
2017-10-24 17:30:54 -0400

    update the game

--- TextTwist/TextTwistModel.swift
+++ TextTwist/TextTwistModel.swift
@@ -7,6 +7,22 @@
 //
 
 import Foundation
-
 class TextTwistModel {
+    var allData = WordData.allInfo
+    var wordletter = WordData.allInfo[0].letters
+    var randomWordletter = ""
+    var randomWordCount = 0
+    var randomWordWords = WordData.allInfo[0].words
+    func getRandomWord(){
+        let random = Int(arc4random_uniform(UInt32(allData.count-1)))
+        let randomData = allData[random]
+        randomWordletter = randomData.letters
+        randomWordCount = randomData.wordCount
+        randomWordWords = randomData.words
+    }
+
+    func checkWord(inputText:String)->Bool{
+        return randomWordWords.contains(inputText)
+    }
+
 }
--- TextTwist/ViewController.swift
+++ TextTwist/ViewController.swift
@@ -8,7 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var message: UILabel!
+    
+    
+    @IBOutlet weak var ChoosenWord: UILabel!
+    
+    @IBOutlet weak var textInputField: UITextField!
+    @IBOutlet weak var textView1: UITextView!
+    
+    @IBOutlet weak var textView2: UITextView!
+    
+    @IBOutlet weak var textView3: UITextView!
+    @IBOutlet weak var textView4: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.textInputField.delegate = self
+        myModel.getRandomWord()
+        ChoosenWord.text = myModel.randomWordletter
+        message.text = "Please enter the words possible combination above"
+    }
+    let myModel = TextTwistModel()
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let stored = textField.text else{
+            return false
+        }
+        guard stored.contains(myModel.randomWordletter) else{
+            return false
+        }
+        if myModel.checkWord(inputText: stored) == true{
+                switch stored.count{
+                case 3:
+                    textView1.text.append(stored + " ")
+                    message.text = "that is right"
+                textField.resignFirstResponder()
+                case 4:
+                    textView2.text.append(stored + " ")
+                    message.text = "that is right"
+                    textField.resignFirstResponder()
+                case 5:
+                    textView3.text.append(stored + " ")
+                    message.text = "that is right"
+                    textField.resignFirstResponder()
+                    
+                default:
+                    textView4.text.append(stored + " ")
+                    message.text = "that is right"
+                    textField.resignFirstResponder()
+            }
+            
+        }
+        else{
+            message.text = "\(stored) is not valid, the word is \(myModel.randomWordletter)"
+            return false
+        }
+        return true
+    }
+    
 }
 
