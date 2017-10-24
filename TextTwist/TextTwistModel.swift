fd90cfea2941cf39677b7af096d912d0a83c35af
Diego Baca <dbaca1025@gmail.com>
2017-10-24 17:31:38 -0400

    started working

--- TextTwist/TextTwistModel.swift
+++ TextTwist/TextTwistModel.swift
@@ -9,4 +9,23 @@
 import Foundation
 
 class TextTwistModel {
+    let correctWords: Set<String>
+    let acceptableCharacters: [Character]
+    var guessesSoFar: Set<String> = Set<String>()
+    init(){
+        let tempTextTwistInfo = WordData.allInfo[Int(arc4random_uniform(UInt32(WordData.allInfo.count)))]
+        correctWords = Set(tempTextTwistInfo.words)
+        acceptableCharacters = Array(tempTextTwistInfo.letters)
+    }
+    
+    //public api
+    func guess(word: String) {
+        
+        
+        
+    }
+    
+    //private api
+    
+    
 }
