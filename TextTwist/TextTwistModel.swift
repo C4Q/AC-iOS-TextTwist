29b424bd7c4cb0de9e7611b61684263dee51938b
Nicole Souvenir <github@nicolesouvenir.com>
2017-10-25 10:03:50 -0400

    First commit

--- TextTwist/TextTwistModel.swift
+++ TextTwist/TextTwistModel.swift
@@ -8,5 +8,40 @@
 
 import Foundation
 
+//handles a specific number of validation answers
+enum letterValidation{
+    case correct, wrong, alreadyGuessed
+}
+
 class TextTwistModel {
+    var words = WordData()
+    var currentWord: String = ""
+    var currentWordGuess: String = ""
+    var currentWordList = WordData.allInfo[0]//grabbing the text twist list from data.swift
+    var randomWord: [String]
+    var currentInfo: TestTwistInfo
+    var availableLetterBank: String //is equal to amount of available letters in random word
+    var usedLetterBank: String
+    var currentLetterGuess: Character = " "
+    init(){
+        currentInfo = WordData.allInfo[Int(arc4random_uniform(3))] //these are all the letters on allInfo
+        randomWord = currentInfo.words
+        availableLetterBank = currentInfo.letters
+        usedLetterBank = currentInfo.letters
+        
+    }
+    //public api
+    
+    //validating guess
+    func guess(_ currentWordGuess: String) -> letterValidation {
+        
+        if !randomWord.contains(currentWordGuess){
+            return .wrong
+        }
+        return .correct
+    }
+
+    //private api : anything not being used by the view controller
+    
+    
 }
--- TextTwist/ViewController.swift
+++ TextTwist/ViewController.swift
@@ -8,7 +8,88 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var usedLetterBank: UILabel!
+    @IBOutlet weak var availableLetterBank: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var wordJumble: UILabel!
+    @IBOutlet weak var guessText: UITextField!
+    
+    @IBOutlet weak var threeWordTextView: UITextView!
+    @IBOutlet weak var fourWordTextView: UITextView!
+    @IBOutlet weak var fiveWordTextView: UITextView!
+    @IBOutlet weak var sixWordTextView: UITextView!
+    
+    var gameRules = TextTwistModel()
+    
+    //setting up func to conform to UITFdelegate
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guessText.delegate = self
+        messageLabel.isHidden = true
+        wordJumble.text = gameRules.currentInfo.letters //replace word jumble label with random word
+        availableLetterBank.text = "Available letters: \(gameRules.currentInfo.letters)"
+        //usedLetterBank.text = "Used letters: \(gameRules.currentInfo.letters)"
+    }
+    
+    //checks every letter
+    //string --. is the letter you enter
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if !gameRules.availableLetterBank.contains(gameRules.currentLetterGuess) {
+            messageLabel.text = "Can't use \(string)."
+            messageLabel.isHidden = false
+            return true
+        }
+        return false
+    }
+    
+    //textfield return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(gameRules.currentWordList)
+        gameRules.currentWordGuess = textField.text!
+        //make sure there is text in the text field otherwise return false
+        guard let text = textField.text else{
+            return false
+        }
+//        guard (3...6).contains(gameRules.currentWord.count) else {
+//            messageLabel.isHidden = false
+//            messageLabel.text = "Word must be 3,4,5 or 6 letters long."
+//            return true
+//        }
+//
+    
+        //text --> entire string when user presses enter
+        switch gameRules.guess(text) {
+        case .correct:
+            messageLabel.text = "Correct!"
+            messageLabel.isHidden = false
+            //putting word in appropriate textview box
+            switch gameRules.currentWordGuess.count {
+            case 3:
+                threeWordTextView.text.append(gameRules.currentWordGuess + "\n")
+            case 4:
+                fourWordTextView.text.append(gameRules.currentWordGuess + "\n")
+            case 5:
+                fiveWordTextView.text.append(gameRules.currentWordGuess + "\n")
+            case 6:
+                sixWordTextView.text.append(gameRules.currentWordGuess + "\n")
+            default:
+                messageLabel.text = "Try Again."
+            }
+        case .wrong:
+            messageLabel.text = "Wrong!"
+            messageLabel.isHidden = false
+        case .alreadyGuessed:
+            messageLabel.text = "Word already guessed. Choose another word."
+            messageLabel.isHidden = false
+        }
 
-}
+    
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}// end of class
 
