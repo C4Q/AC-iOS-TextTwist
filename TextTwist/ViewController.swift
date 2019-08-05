222d8bb39cb41f36424244f2c5f65f32e18388fb
Jason Ruan <jasonruan@pursuit.org>
2019-08-05 17:05:25 -0400

    Adds function check if user guess is correct and then adds guess to respective text view

--- TextTwist/ViewController.swift
+++ TextTwist/ViewController.swift
@@ -9,8 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
+    
     var sample = TextTwistModel()
+    var threeLetterAnswers = ""
+    var fourLetterAnswers = ""
+    var fiveLetterAnswers = ""
+    var sixLetterAnswers = ""
     
     @IBOutlet weak var letterBank: UILabel!
     @IBOutlet weak var validationMessage: UILabel!
@@ -24,6 +28,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBAction func guessInput(_ sender: UITextField) {
         if sample.checkGuess(sender.text!) {
             validationMessage.text = "Correct!"
+            sortGuessIntoWordBank(sender.text!)
+        } else {
+            validationMessage.text = "Wrong! Try Again"
         }
     }
     
@@ -32,11 +39,31 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         userGuess.delegate = self
         validationMessage.text = ""
+        showRandomLetters()
     }
     
+    func sortGuessIntoWordBank(_ guess: String) {
+        switch guess.count {
+        case 3:
+            threeLetterAnswers += "\(guess)\n"
+            threeLetters.text = threeLetterAnswers
+        case 4:
+            fourLetterAnswers += "\(guess)\n"
+            fourLetters.text = fourLetterAnswers
+        case 5:
+            fiveLetterAnswers += "\(guess)\n"
+            fiveLetters.text = fiveLetterAnswers
+        case 6:
+            sixLetterAnswers += "\(guess)\n"
+            sixLetters.text = sixLetterAnswers
+        default:
+            print("Never gonna give you up")
+        }
+    }
     
-    
-    
+    func showRandomLetters() {
+        letterBank.text = sample.testCase.letters
+    }
     
     
     
