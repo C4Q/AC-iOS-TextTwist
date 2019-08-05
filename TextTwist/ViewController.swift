1a0467dfe2573619aa0d807e6f52da41d0fd7d10
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-05 15:42:56 -0400

    created IB outlets and IB actions from objects

--- TextTwist/ViewController.swift
+++ TextTwist/ViewController.swift
@@ -8,14 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet var wordBanks: [UITextView]!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    
+        textField.delegate = self
     }
+    
+    
+    @IBAction func guessWords(_ sender: UITextField) {
+    }
+    
+    
+    
 }
 
