40edc82445eae7926f84998fa325e456be29e1de
NormanLiana <liananorman@pursuit.org>
2019-08-08 19:31:43 -0400

    Makes VC conform to UITextFieldDelegate, adds outlets, adds currentGame var

--- TextTwist/ViewController.swift
+++ TextTwist/ViewController.swift
@@ -8,7 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    //let wordData = WordData()
+    // we don't use an instance of WordData, instead, with the static we would go to the model and make a function/method
+    var currentGame: TextTwistInfo? {
+        didSet {
+            scrambledLetters.text = self.currentGame?.letters
+        }
+    }
+    
+    @IBOutlet weak var userGuessMessageLabel: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    @IBOutlet weak var threeLetterTextView: UITextView!
+    @IBOutlet weak var fourLetterTextView: UITextView!
+    @IBOutlet weak var fiveLetterTextView: UITextView!
+    @IBOutlet weak var sixLetterTextView: UITextView!
 
     @IBOutlet weak var scrambledLetters: UILabel!
     
